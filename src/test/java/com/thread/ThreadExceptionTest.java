468ce639d8e626ec73ea154ecd7df53c15ca2ae4
sunjx <sunjx1988@163.com>
2017-11-03 15:43:25 +0800

    线程中出现异常的处理

---
+++ src/test/java/com/thread/ThreadExceptionTest.java
@@ -0,0 +1,30 @@
+package com.thread;
+
+/**
+ * Created by sunjx on 2017/11/3.
+ */
+public class ThreadExceptionTest {
+
+    public static void main(String[] args) {
+
+        Thread thread = new Thread();
+
+        //在线程中使用UncaughtExceptionHandler 对指定线程中的异常进行处理
+        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                //dosomething
+                e.printStackTrace();
+            }
+        });
+
+        //对所有的线程异常进行处理
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                //dosomething
+                e.printStackTrace();
+            }
+        });
+    }
+}
